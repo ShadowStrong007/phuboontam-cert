8932a30ae94412d46ce49490907f9e8c4eddfe23
Jack Hidden <metee.boon.jack@gmail.com>
2023-01-28 23:36:39 +0700

    [Adjust] หน้า welcome

--- pbt-app/src/views/pages/Home.jsx
+++ pbt-app/src/views/pages/Home.jsx
@@ -29,32 +29,11 @@ export default class Home extends Component {
             </div>
             <div class="mt-4 content-desc">
               {/* <!-- Content Detail Here --> */}
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply
-              random text. It has roots in a piece of classical Latin literature
-              from 45 BC, making it over 2000 years old. Richard McClintock, a
-              Latin professor at Hampden-Sydney College in Virginia, looked up
-              one of the more obscure Latin words, consectetur, from a Lorem
-              Ipsum passage, and going through the cites of the word in
-              classical literature, discovered the undoubtable source. Lorem
-              Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-              Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-              written in 45 BC. This book is a treatise on the theory of ethics,
-              very popular during the Renaissance. The first line of Lorem
-              Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-              section 1.10.32. The standard chunk of Lorem Ipsum used since the
-              1500s is reproduced below for those interested. Sections 1.10.32
-              and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are
-              also reproduced in their exact original form, accompanied by
-              English versions from the 1914 translation by H. Rackham.
+              ยินดีต้อนรับท่านเข้าสู่ หจก.ภูบุญเต็ม เซ็นเตอร์ https://pbtcenters.com/<br/>
+              รับทำกรอบรูปทั้งปลีก และ ส่ง พิมพ์โลโก้ พิมพ์รูปภาพลงแก้วกาแฟ เซรามิค รับทำป้ายตัวอักษรจากแผ่นพลาสวูด <br/>รับจัดตกแต่งงานมงคลครบวงจร งานแกะสลัก ออกบัตรรับรองพระแท้ ทรัพย์อจินไตย วัตถุมงคลต่างๆ อัญมณีนาคราช
+              <br/>สนใจติดต่อแผนกออกบัตรรับรอง และ ทรัพย์อจินไตย <br/>โทร.02-001-4809,02-001-6460,095-4142495
+              แผนกกรอบรูป <br/>ป้ายและอื่นๆ โทร.042-751129,065-085-3142,<br/>
+              สำนักงานใหญ่ตั้งอยู่ที่ ที่91 หมู่ที่ 4 ต.ธาตุนาเวง  อ.เมือง จ.สกลนคร  47000
             </div>
           </div>
           <div id="about-me" class="mt-4 about-me">
